52d64b7c03f414bd2cb3b1f5fd1438b98d774e9e
dalem051198 <dalem051198@gmail.com>
2024-11-14 19:15:06 -0500

    auto commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default ({
+  // compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  components: true,
+  runtimeConfig: {
+    public: {
+      botId: process.env.NUXT_APP_BOT_ID,
+      chatId: process.env.NUXT_APP_CHAT_ID,
+    }
+  }
+})
